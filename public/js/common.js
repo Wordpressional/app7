fbcbdaeb69c4eca61188c24d50445de1b7079e9a
Nagashwini <nagashwini@pyrumas.com>
2018-11-20 14:21:28 +0530

    prompt js code added

--- public/js/common.js
+++ public/js/common.js
@@ -172,6 +172,18 @@ if ('serviceWorker' in navigator) {
       });
   }
 
+  var deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (e) {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  showAddToHomeScreen();
+
+});
+
 });
 
  
@@ -191,17 +203,7 @@ $('#blogCarousel').carousel({
 });
 
 
-var deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', function (e) {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-
-  showAddToHomeScreen();
-
-});
 
 function addToHomeScreen() {  var a2hsBtn = document.querySelector(".ad2hs-prompt");  // hide our user interface that shows our A2HS button
   a2hsBtn.style.display = 'none';  // Show the prompt
