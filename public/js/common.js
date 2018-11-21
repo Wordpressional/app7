a8a7fa1027685915a3b3745ce4efbe28471c7bc4
Nagashwini <nagashwini@pyrumas.com>
2018-11-21 11:53:53 +0530

    service worker error

--- public/js/common.js
+++ public/js/common.js
@@ -172,23 +172,13 @@ if ('serviceWorker' in navigator) {
       });
   }
 
-  var deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', function (e) {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-
-  showAddToHomeScreen();
-
-});
+ 
 
 });
 
  
        
-var $button = document.querySelector('.icolist .button');
+var $button = document.querySelector('.icolist', '.button');
 $button.addEventListener('click', function() {
   var duration = 0.3,
       delay = 0.08;
@@ -205,28 +195,3 @@ $('#blogCarousel').carousel({
 
 
 
-function addToHomeScreen() {  var a2hsBtn = document.querySelector(".ad2hs-prompt");  // hide our user interface that shows our A2HS button
-  a2hsBtn.style.display = 'none';  // Show the prompt
-  deferredPrompt.prompt();  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then(function(choiceResult){
-
-  if (choiceResult.outcome === 'accepted') {
-    console.log('User accepted the A2HS prompt');
-  } else {
-    console.log('User dismissed the A2HS prompt');
-  }
-
-  deferredPrompt = null;
-
-});}
-
-function showAddToHomeScreen() {
-
-  var a2hsBtn = document.querySelector(".ad2hs-prompt");
-
-  a2hsBtn.style.display = "block";
-
-  a2hsBtn.addEventListener("click", addToHomeScreen);
-
-}  
