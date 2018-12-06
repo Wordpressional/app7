056007b8e6c1ce479ae2aa9582a729becd5ec77e
Nagashwini <nagashwini@pyrumas.com>
2018-12-06 11:06:37 +0530

    check manifest scope

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
     <!-- Styles -->
     <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet"/>
+     <link rel="manifest" href="{{url('/manifest.json')}}">
+   <link rel="manifest" href="{{url('/manifest.webmanifest')}}">
       @yield('css')
 </head>
 <body class="bg-light">
@@ -79,6 +81,30 @@
         $(".inputPassIcon2").css("color", "blue");
         });
     });
+</script>
+<script src="{{ asset('upup.min.js') }}"></script>
+<script src="{{ asset('upup.sw.min.js') }}"></script>
+<script>
+UpUp.start({
+  'content': 'You are Offline. Cannot reach site. Please check your internet connection.',
+  'service-worker-url': "{{ asset('upup.sw.min.js') }}"
+});
+
+$(document).ready(function() {
+
+if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register("{{ asset('upup.sw.min.js') }}")
+      .then(function(reg){
+        console.log("Yes, it did.");
+      }).catch(function(err) {
+        console.log("No it didn't. This happened: ", err)
+      });
+  }
+
+ 
+
+});
 </script>
     @stack('inline-scripts')
 </body>
