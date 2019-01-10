754ae845d159062b614b0d52d89c2db3f246d271
nagashwini <nagashwini@pyrumas.com>
2019-01-10 12:09:55 +0000

    custom13

---
+++ resources/views/shortcodes/custom/customplain_13.blade.php
@@ -0,0 +1,70 @@
+<!-- Footer -->
+    <div class="mlthemeone footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="footer-col">
+                        <h4>About Cedo</h4>
+                        <p>We're passionate about offering the best financial services to our loyal customers</p>
+                    </div>
+                </div> <!-- end of col -->
+                <div class="col-md-4">
+                    <div class="footer-col mx-auto">
+                        <h4>Important Links</h4>
+                        <p><a class="underline" href="terms-conditions.html">Terms & Conditions</a>, <a class="underline" href="privacy-policy.html">Privacy Policy</a>, <a class="underline" href="#your-link">Institute Of Credit & Loans Control</a>, <a class="underline" href="#your-link">National Bank</a></p>
+                    </div>
+                </div> <!-- end of col -->
+                <div class="col-md-4">
+                    <div class="footer-col float-lg-right">
+                        <h4>Social Media</h4>
+                        <span class="fa-stack fa-lg">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x facebook"></i>
+                                <i class="fab fa-facebook-f fa-stack-1x"></i>
+                            </a>
+                        </span>
+                        <span class="fa-stack fa-lg">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x twitter"></i>
+                                <i class="fab fa-twitter fa-stack-1x"></i>
+                            </a>
+                        </span>
+                        <span class="fa-stack fa-lg">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x google-plus"></i>
+                                <i class="fab fa-google-plus-g fa-stack-1x"></i>
+                            </a>
+                        </span>
+                        <span class="fa-stack fa-lg">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x instagram"></i>
+                                <i class="fab fa-instagram fa-stack-1x"></i>
+                            </a>
+                        </span>
+                        <span class="fa-stack fa-lg">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x linkedin"></i>
+                                <i class="fab fa-linkedin-in fa-stack-1x"></i>
+                            </a>
+                        </span>
+                        <span class="fa-stack fa-lg">
+                            <a href="#your-link">
+                                <i class="fas fa-circle fa-stack-2x dribbble"></i>
+                                <i class="fab fa-dribbble fa-stack-1x"></i>
+                            </a>
+                        </span>
+                    </div> 
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+        <div class="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center">Copyright © Cedo - Loans Credit Landing Page Template by <a class="underline" href="main.html">Inovatik</a></p>
+                    </div> <!-- end of col -->
+                </div> <!-- enf of row -->
+            </div> <!-- end of container -->
+        </div> <!-- end of copyright -->
+    </div> <!-- end of footer -->  
+    <!-- end of footer -->
