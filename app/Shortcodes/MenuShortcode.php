2690978e34d54e57ba13b035d1af6f718fc53e82
Nagashwini <nagashwini@pyrumas.com>
2019-01-20 01:12:10 +0530

    menu builder page added

---
+++ app/Shortcodes/MenuShortcode.php
@@ -0,0 +1,38 @@
+<?php 
+//https://github.com/harimayco/wmenu-builder
+namespace App\Shortcodes;
+use App\Category;
+use App\Post;
+
+use Harimayco\Menu\Facades\Menu;
+
+use Harimayco\Menu\Models\Menus;
+use Harimayco\Menu\Models\MenuItems;
+
+
+
+class MenuShortcode {
+
+  public function menulist($shortcode, $content, $compiler, $name, $viewData)
+  {
+  	$menuList = Menu::getByName($content);
+
+    //dd($posts);
+    return view('shortcodes.menus.topmenu')->with('menuList', $menuList);
+  }
+  
+
+
+
+  public function responsivemenulist($shortcode, $content, $compiler, $name, $viewData)
+  {
+  	$menuList = Menu::getByName($content);
+
+    //dd($posts);
+    return view('shortcodes.menus.responsivemenu')->with('menuList', $menuList);
+  }
+
+
+  
+  
+}
