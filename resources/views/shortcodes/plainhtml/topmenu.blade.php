799d411d42473660d8bcdce8022ef5935efddc9c
nagashwini <nagashwini@pyrumas.com>
2019-01-19 19:46:28 +0000

    top menu conflict

--- resources/views/shortcodes/plainhtml/topmenu.blade.php
+++ resources/views/shortcodes/plainhtml/topmenu.blade.php
@@ -1,13 +1,10 @@
-
-
-
 <!--================Header Menu Area =================-->
 
 
-<header class="main_menu_area imgpage" style="position:static; ">
+<header class="main_menu_area" style="position:static; ">
 
-<span class="logospan"><img src="{{ asset('webhome/img/logo.png') }}"  alt="" /></span>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<span class="logospan"><img src="http://139.59.47.15/dynamic/webhome/img/logo.png"  alt="" /></span>
+<nav class="navbar navbar-expand-lg navbar-light">
 <a class="navbar-brand" href="#"></a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span></span>
@@ -17,13 +14,13 @@
 
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a class="nav-link" href="{{ URL::to('/') }}">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ url('/cattype/books') }}">ICO Services&nbsp;&nbsp;&nbsp;<span class="icon-cogw"><i class="fa fa-angle-down"></i></span></a></li>
+        <li class="nav-item"><a class="nav-link" href="http://139.59.47.15/dynamic">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="http://139.59.47.15/dynamic/cattype/books">ICO Services   <span class="icon-cogw"><i class="fa fa-angle-down"></i></span></a></li>
        
-        <li class="nav-item"><a class="nav-link" href="{{ url('thispage/events-conferences-and-projects') }}">Promote Your ICO&nbsp;&nbsp;&nbsp;<span class="icon-cogw"><i class="fa fa-angle-down"></i></span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ url('/thispage/contact') }}">About&nbsp;&nbsp;&nbsp;<span class="icon-cogw"><i class="fa fa-angle-down"></i></span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ url('thispage/events-conferences-and-projects') }}"><span class="icon-cog"><i class="fa fa-user"></i></span>&nbsp;&nbsp;&nbsp;Login</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ url('/thispage/contact') }}"><span class="icon-cog"><i class="fa fa-edit"></i></span>&nbsp;&nbsp;&nbsp;Register</a></li>
+        <li class="nav-item"><a class="nav-link" href="http://139.59.47.15/dynamic/thispage/events-conferences-and-projects">Promote Your ICO   <span class="icon-cogw"><i class="fa fa-angle-down"></i></span></a></li>
+        <li class="nav-item"><a class="nav-link" href="http://139.59.47.15/dynamic/thispage/contact">About   <span class="icon-cogw"><i class="fa fa-angle-down"></i></span></a></li>
+        <li class="nav-item"><a class="nav-link" href="http://139.59.47.15/dynamic/thispage/events-conferences-and-projects"><span class="icon-cog"><i class="fa fa-user"></i></span>   Login</a></li>
+        <li class="nav-item"><a class="nav-link" href="http://139.59.47.15/dynamic/thispage/contact"><span class="icon-cog"><i class="fa fa-edit"></i></span>   Register</a></li>
     </ul>
    
 </div>
