636a489f0ae29ca9db4fb11c4dabbe00041cf134
Nagashwini <nagashwini@pyrumas.com>
2019-01-20 12:52:37 +0530

    menu script not working

---
+++ resources/views/admin/layouts/menumaster.blade.php
@@ -0,0 +1,42 @@
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    @include('admin.layouts.compscripts.headstyle')
+    @yield('css')
+
+</head>
+<body class="admin-body">
+    <div class="wrapper">
+
+        <div class="adminltehs">
+        @include('admin.shared.header')
+        @include('admin.shared.sidebar')
+        </div>
+    <div class="content-wrapper bg-light">
+
+    <div id="app">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    @include('shared/alerts')
+                        
+                    <div class="card">
+                        <div class="card-body">
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+
+    @include('admin.layouts.compscripts.menuscript')
+     @yield('menuscripts')   
+    @include('admin.layouts.compscripts.serviceworker')
+    @yield('scripts')
+    
+</body>
+</html>
