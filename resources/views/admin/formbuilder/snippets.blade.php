f92b11cf341c4554699d43e871324d25a1969acf
Nagashwini <nagashwini@pyrumas.com>
2019-01-21 18:17:37 +0530

    bootstrap components responsive

--- resources/views/admin/formbuilder/snippets.blade.php
+++ resources/views/admin/formbuilder/snippets.blade.php
@@ -75,20 +75,20 @@
 <!-------------------------------------------------------------------------------------------------->
 <div data-type="component-text" data-preview="{{asset('examples/snippets/preview/page_header.png')}}" data-keditor-title="Page header" data-keditor-categories="Text;Heading;Bootstrap component">
     <div class="page-header">
-        <h1 style="margin-bottom: 30px; font-size: 50px;"><b class="text-uppercase">Cras justo odio</b> <small>Donec id elit non mi</small></h1>
-        <p class="lead"><em>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</em></p>
+        <h1 style="margin-bottom: 30px; font-size: 50px; padding:20px;"><b class="text-uppercase">Cras justo odio</b> <small>Donec id elit non mi</small></h1>
+        <p class="lead" style="padding:20px;"><em>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</em></p>
     </div>
 </div>
 
 <div data-type="component-text" data-preview="{{asset('examples/snippets/preview/text.png')}}" data-keditor-title="Text block" data-keditor-categories="Text">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro labore architecto fuga tempore omnis aliquid, rerum numquam deleniti ipsam earum velit aliquam deserunt, molestiae officiis mollitia accusantium suscipit fugiat esse magnam eaque cumque, iste corrupti magni? Illo dicta saepe, maiores fugit aliquid consequuntur aut, rem ex iusto dolorem molestias obcaecati eveniet vel voluptatibus recusandae illum, voluptatem! Odit est possimus nesciunt.</p>
+    <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro labore architecto fuga tempore omnis aliquid, rerum numquam deleniti ipsam earum velit aliquam deserunt, molestiae officiis mollitia accusantium suscipit fugiat esse magnam eaque cumque, iste corrupti magni? Illo dicta saepe, maiores fugit aliquid consequuntur aut, rem ex iusto dolorem molestias obcaecati eveniet vel voluptatibus recusandae illum, voluptatem! Odit est possimus nesciunt.</p>
 </div>
 
 <div data-type="component-text" data-preview="{{asset('examples/snippets/preview/jumbotron.png')}}" data-keditor-title="Jumbotron" data-keditor-categories="Text;Heading;Bootstrap component;Dynamic component">
     <div class="jumbotron">
-        <h1>Hello, world!</h1>
-        <p>This is a simple hero unit</p>
-        <p><a role="button" href="#" class="btn btn-primary btn-lg">Learn more</a></p>
+        <h1 style="padding:20px;">Hello, world!</h1>
+        <p style="padding:20px;">This is a simple hero unit</p>
+        <p style="padding:20px;"><a role="button" href="#" class="btn btn-primary btn-lg">Learn more</a></p>
     </div>
 
     <div data-dynamic-href="{{asset('examples/snippets/_dynamic_content.html')}}"></div>
@@ -138,7 +138,7 @@
         <img src="{{asset('examples/snippets/img/somewhere_bangladesh.jpg')}}" width="100%" height="" />
         <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p style="color:black; padding:20px;">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
             <p>
                 <a href="#" class="btn btn-primary" role="button">Button</a>
                 <a href="#" class="btn btn-default" role="button">Button</a>
@@ -148,25 +148,25 @@
 </div>
 
 <div data-type="component-text" data-preview="{{asset('examples/snippets/preview/heading_1.png')}}" data-keditor-title="Heading 1" data-keditor-categories="Text;Heading">
-    <h1>Heading text 1</h1>
-    <p>Body text</p>
+    <h1 style="padding:20px;">Heading text 1</h1>
+    <p style="padding:20px;">Body text</p>
 </div>
 
 <div data-type="component-text" data-preview="{{asset('examples/snippets/preview/heading_2.png')}}" data-keditor-title="Heading 2" data-keditor-categories="Text;Heading">
-    <h2>Heading text 2</h2>
-    <p>Body text</p>
+    <h2 style="padding:20px;">Heading text 2</h2>
+    <p style="padding:20px;">Body text</p>
 </div>
 
 <div data-type="component-text" data-preview="{{asset('examples/snippets/preview/media_panel.png')}}" data-keditor-title="Media Panel" data-keditor-categories="Media;Photo;Bootstrap component">
-    <div class="media">
-        <div class="media-left">
+    <div class="media col-sm-12">
+        <div class="media-left col-md-6 col-sm-12 col-xs-12">
             <a href="#">
-                <img class="media-object" src="{{asset('examples/snippets/img/yenbai_vietnam.jpg')}}" width="150" height="" />
+                <img class="media-object col-sm-12 col-xs-12" src="{{asset('examples/snippets/img/yenbai_vietnam.jpg')}}" width="150" height="" />
             </a>
         </div>
-        <div class="media-body">
-            <h4 class="media-heading">Media heading</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos minus hic praesentium, nihil nemo, optio delectus explicabo at beatae. Ullam itaque, officiis maxime quibusdam impedit vero?</p>
+        <div class="media-body col-md-6 col-sm-12 col-xs-12">
+            <h4 class="media-heading" style="padding:20px;">Media heading</h4>
+            <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos minus hic praesentium, nihil nemo, optio delectus explicabo at beatae. Ullam itaque, officiis maxime quibusdam impedit vero?</p>
         </div>
     </div>
 </div>
@@ -177,8 +177,8 @@
             <img class="img-circle img-responsive" style="display: inline-block;" src="{{asset('examples/snippets/img/sydney_australia_squared.jpg')}}" />
         </div>
         <div class="col-md-6">
-            <h3>Lorem ipsum</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi molestias eius quaerat, adipisci ratione aliquid eum explicabo illum temporibus? Optio facilis eveniet quam, impedit eos architecto sequi dolorum illo facere, consequatur sit voluptatibus sunt eius ad officia corrupti modi quia minima voluptas vero. Minus, maxime!</p>
+            <h3 style="padding:20px; text-align:center;">Lorem ipsum</h3>
+            <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi molestias eius quaerat, adipisci ratione aliquid eum explicabo illum temporibus? Optio facilis eveniet quam, impedit eos architecto sequi dolorum illo facere, consequatur sit voluptatibus sunt eius ad officia corrupti modi quia minima voluptas vero. Minus, maxime!</p>
         </div>
     </div>
 </div>
@@ -187,18 +187,18 @@
     <div class="row">
         <div class="col-md-4 text-center">
             <img class="img-circle img-responsive" style="display: inline-block;" src="{{asset('examples/snippets/img/somewhere_bangladesh_squared.jpg')}}" />
-            <h3>Lorem ipsum</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, alias, temporibus? Vero natus modi ipsa debitis, accusamus accusantium cum quam. Saepe atque quisquam pariatur voluptatem expedita nesciunt reprehenderit et vitae.</p>
+            <h3 style="padding:20px;">Lorem ipsum</h3>
+            <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, alias, temporibus? Vero natus modi ipsa debitis, accusamus accusantium cum quam. Saepe atque quisquam pariatur voluptatem expedita nesciunt reprehenderit et vitae.</p>
         </div>
         <div class="col-md-4 text-center">
             <img class="img-circle img-responsive" style="display: inline-block;" src="{{asset('examples/snippets/img/wellington_newzealand_squared.jpg')}}" />
-            <h3>Lorem ipsum</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, aut, earum. Quod, debitis, delectus. Maxime eius ipsam sit dolorum perspiciatis obcaecati consectetur, explicabo reprehenderit repellat tempore veniam eos ducimus! Dignissimos.</p>
+            <h3 style="padding:20px;">Lorem ipsum</h3>
+            <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, aut, earum. Quod, debitis, delectus. Maxime eius ipsam sit dolorum perspiciatis obcaecati consectetur, explicabo reprehenderit repellat tempore veniam eos ducimus! Dignissimos.</p>
         </div>
         <div class="col-md-4 text-center">
             <img class="img-circle img-responsive" style="display: inline-block;" src="{{asset('examples/snippets/img/yenbai_vietnam_squared.jpg')}}" />
-            <h3>Lorem ipsum</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil voluptatibus dicta corrupti aliquam, natus voluptatem pariatur quidem nostrum nisi corporis id ratione exercitationem et recusandae incidunt assumenda soluta qui odit.</p>
+            <h3 style="padding:20px;">Lorem ipsum</h3>
+            <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil voluptatibus dicta corrupti aliquam, natus voluptatem pariatur quidem nostrum nisi corporis id ratione exercitationem et recusandae incidunt assumenda soluta qui odit.</p>
         </div>
     </div>
 </div>
@@ -206,7 +206,7 @@
 <!--<div data-type="component-form" data-preview="{{asset('examples/snippets/preview/form.png')}}" data-keditor-title="Bootstrap Form" data-keditor-categories="Form;Bootstrap component"></div>-->
 
 <div data-type="component-nonExisting" data-preview="{{asset('examples/snippets/preview/text.png')}}" data-website="website01" data-blog="blog01" data-article="article01" data-tags="tag01,tag02" data-keditor-title="Text block with dynamic content" data-keditor-categories="Text;Dynamic component">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro labore architecto fuga tempore omnis aliquid, rerum numquam deleniti ipsam earum velit aliquam deserunt, molestiae officiis mollitia accusantium suscipit fugiat esse magnam eaque cumque, iste corrupti magni? Illo dicta saepe, maiores fugit aliquid consequuntur aut, rem ex iusto dolorem molestias obcaecati eveniet vel voluptatibus recusandae illum, voluptatem! Odit est possimus nesciunt.</p>
+    <p style="padding:20px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro labore architecto fuga tempore omnis aliquid, rerum numquam deleniti ipsam earum velit aliquam deserunt, molestiae officiis mollitia accusantium suscipit fugiat esse magnam eaque cumque, iste corrupti magni? Illo dicta saepe, maiores fugit aliquid consequuntur aut, rem ex iusto dolorem molestias obcaecati eveniet vel voluptatibus recusandae illum, voluptatem! Odit est possimus nesciunt.</p>
     <div data-dynamic-href="{{asset('examples/snippets/_dynamic_content')}}"></div>
 </div>
 
