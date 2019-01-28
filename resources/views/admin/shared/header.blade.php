81bffe7771c186228ede84083013240d66c8bc7b
Nagashwini <nagashwini@pyrumas.com>
2019-01-28 17:28:39 +0530

    logo

--- resources/views/admin/shared/header.blade.php
+++ resources/views/admin/shared/header.blade.php
@@ -3,7 +3,7 @@
 <header class="main-header">
     <a href="" class="logo">
         <!--<span class="logo-mini"><b>Symbol</b></span>-->
-        <span class="logo-lg"><b><img src="{{ asset($data['n_companyname']->clogo) }}" alt="logo"></b></span>
+        <span class="logo-lg"><b>@if($data['n_companyname'])<img src="{{ asset($data['n_companyname']->clogo) }}" alt="logo">@else @endif</b></span>
     </a>
     <nav class="navbar navbar-static-top">
         <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
@@ -15,7 +15,7 @@
             <ul class="nav navbar-nav">
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        @if($data)
+                        @if($data['n_companyname'])
                         <img src="{{ asset($data['n_companyname']->defaultprofileimg) }}" class="user-image" alt="User Image" />
                         @endif
                         <span class="hidden-xs">@if($data){{$data['n_loggeduser']}}@endif</span>
@@ -23,10 +23,10 @@
                     <ul class="dropdown-menu">
                     <!-- User image -->
                         <li class="user-header">
-                             @if($data)
+                             @if($data['n_companyname'])
                             <img src="{{ asset($data['n_companyname']->defaultprofileimg) }}" class="img-circle" alt="User Image" />@endif
                             <p>
-                               @if($data){{$data['n_loggeduser']}}@endif
+                               @if($data['n_companyname']){{$data['n_loggeduser']}}@endif
                             </p>
                         </li>
                         <li class="user-footer">
