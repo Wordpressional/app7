68822fb175fe799fa716d1d13be5d0f79ec187f4
Nagashwini <nagashwini@pyrumas.com>
2019-01-28 17:09:14 +0530

    default favicon

--- resources/views/admin/layouts/compscripts/headstyle.blade.php
+++ resources/views/admin/layouts/compscripts/headstyle.blade.php
@@ -4,6 +4,8 @@
 
 @if($data)
 <link rel="icon" href="{{asset($data['n_companyname']->favicon)}}" type="image/x-icon" />
+@else
+<link rel="icon" href="/favicon.ico" type="image/x-icon" />
 @endif
 
 <!-- CSRF Token -->
