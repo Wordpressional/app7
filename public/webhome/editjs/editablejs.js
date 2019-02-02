d857d69973341baa557811f7b848249c7cc65739
nagashwini <nagashwini@pyrumas.com>
2019-02-02 13:28:54 +0000

    old changes

--- public/webhome/editjs/editablejs.js
+++ public/webhome/editjs/editablejs.js
@@ -1,25 +1,765 @@
-$(document).ready( function() {
-    $('#myCarousel').carousel({
-		interval:   4000
-	});
-	
-	var clickEvent = false;
-	$('#myCarousel').on('click', '.nav a', function() {
-			clickEvent = true;
-			$('.nav li').removeClass('active');
-			$(this).parent().addClass('active');		
-	}).on('slid.bs.carousel', function(e) {
-		if(!clickEvent) {
-			var count = $('.nav').children().length -1;
-			var current = $('.nav li.active');
-			current.removeClass('active').next().addClass('active');
-			var id = parseInt(current.data('slide-to'));
-			if(count == id) {
-				$('.nav li').first().addClass('active');	
-			}
-		}
-		clickEvent = false;
-	});
-	
+let modalId = $('#image-gallery');
+
+$(document)
+  .ready(function () {
+
+    loadGallery(true, 'a.thumbnail');
+
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+      $('#show-previous-image, #show-next-image')
+        .show();
+      if (counter_max === counter_current) {
+        $('#show-next-image')
+          .hide();
+      } else if (counter_current === 1) {
+        $('#show-previous-image')
+          .hide();
+      }
+    }
+
+    /**
+     *
+     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+     * @param setClickAttr  Sets the attribute for the click handler.
+     */
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#show-next-image, #show-previous-image')
+        .click(function () {
+          if ($(this)
+            .attr('id') === 'show-previous-image') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#image-gallery-title')
+          .text($sel.data('title'));
+        $('#image-gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
+    }
+  });
+
+// build key actions
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37: // left
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+          $('#show-previous-image')
+            .click();
+        }
+        break;
+
+      case 39: // right
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+          $('#show-next-image')
+            .click();
+        }
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage1").hide();
+
+$( ".boxsnipetimage1" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage1").hasClass("editable")) {
+    $(".editsnipetimage1").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage1").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage1").hide();
 
 });
+
+ $(".editsnipetimage1").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage1").addClass("editable");
+   $(".editsnipetimage1").hide();
+  $(".savesnipetimage1").show();
+  $(".imageUploadsnipetimage1").show();
+});
+
+$(".savesnipetimage1").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage1").removeClass("editable");
+ 
+  $(".editsnipetimage1").hide();
+  $(".imageUploadsnipetimage1").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage1").change(function() {
+
+    readURLsnipetimg1(this);
+});
+
+});
+function readURLsnipetimg1(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage1').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage1a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage2").hide();
+
+$( ".boxsnipetimage2" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage2").hasClass("editable")) {
+    $(".editsnipetimage2").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage2").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage2").hide();
+
+});
+
+ $(".editsnipetimage2").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage2").addClass("editable");
+   $(".editsnipetimage2").hide();
+  $(".savesnipetimage2").show();
+  $(".imageUploadsnipetimage2").show();
+});
+
+$(".savesnipetimage2").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage2").removeClass("editable");
+ 
+  $(".editsnipetimage2").hide();
+  $(".imageUploadsnipetimage2").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage2").change(function() {
+
+    readURLsnipetimg2(this);
+});
+
+});
+function readURLsnipetimg2(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage2').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage2a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage3").hide();
+
+$( ".boxsnipetimage3" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage3").hasClass("editable")) {
+    $(".editsnipetimage3").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage3").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage3").hide();
+
+});
+
+ $(".editsnipetimage3").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage3").addClass("editable");
+   $(".editsnipetimage3").hide();
+  $(".savesnipetimage3").show();
+  $(".imageUploadsnipetimage3").show();
+});
+
+$(".savesnipetimage3").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage3").removeClass("editable");
+ 
+  $(".editsnipetimage3").hide();
+  $(".imageUploadsnipetimage3").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage3").change(function() {
+
+    readURLsnipetimg3(this);
+});
+
+});
+function readURLsnipetimg3(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage3').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage3a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage4").hide();
+
+$( ".boxsnipetimage4" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage4").hasClass("editable")) {
+    $(".editsnipetimage4").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage4").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage4").hide();
+
+});
+
+ $(".editsnipetimage4").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage4").addClass("editable");
+   $(".editsnipetimage4").hide();
+  $(".savesnipetimage4").show();
+  $(".imageUploadsnipetimage4").show();
+});
+
+$(".savesnipetimage4").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage4").removeClass("editable");
+ 
+  $(".editsnipetimage4").hide();
+  $(".imageUploadsnipetimage4").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage4").change(function() {
+
+    readURLsnipetimg4(this);
+});
+
+});
+function readURLsnipetimg4(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage4').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage4a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage5").hide();
+
+$( ".boxsnipetimage5" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage5").hasClass("editable")) {
+    $(".editsnipetimage5").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage5").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage5").hide();
+
+});
+
+ $(".editsnipetimage5").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage5").addClass("editable");
+   $(".editsnipetimage5").hide();
+  $(".savesnipetimage5").show();
+  $(".imageUploadsnipetimage5").show();
+});
+
+$(".savesnipetimage5").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage5").removeClass("editable");
+ 
+  $(".editsnipetimage5").hide();
+  $(".imageUploadsnipetimage5").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage5").change(function() {
+
+    readURLsnipetimg5(this);
+});
+
+});
+function readURLsnipetimg5(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage5').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage5a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage6").hide();
+
+$( ".boxsnipetimage6" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage6").hasClass("editable")) {
+    $(".editsnipetimage6").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage6").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage6").hide();
+
+});
+
+ $(".editsnipetimage6").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage6").addClass("editable");
+   $(".editsnipetimage6").hide();
+  $(".savesnipetimage6").show();
+  $(".imageUploadsnipetimage6").show();
+});
+
+$(".savesnipetimage6").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage6").removeClass("editable");
+ 
+  $(".editsnipetimage6").hide();
+  $(".imageUploadsnipetimage6").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage6").change(function() {
+
+    readURLsnipetimg6(this);
+});
+
+});
+function readURLsnipetimg6(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage6').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage6a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage7").hide();
+
+$( ".boxsnipetimage7" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage7").hasClass("editable")) {
+    $(".editsnipetimage7").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage7").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage7").hide();
+
+});
+
+ $(".editsnipetimage7").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage7").addClass("editable");
+   $(".editsnipetimage7").hide();
+  $(".savesnipetimage7").show();
+  $(".imageUploadsnipetimage7").show();
+});
+
+$(".savesnipetimage7").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage7").removeClass("editable");
+ 
+  $(".editsnipetimage7").hide();
+  $(".imageUploadsnipetimage7").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage7").change(function() {
+
+    readURLsnipetimg7(this);
+});
+
+});
+function readURLsnipetimg7(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage7').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage7a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage8").hide();
+
+$( ".boxsnipetimage8" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage8").hasClass("editable")) {
+    $(".editsnipetimage8").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage8").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage8").hide();
+
+});
+
+ $(".editsnipetimage8").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage8").addClass("editable");
+   $(".editsnipetimage8").hide();
+  $(".savesnipetimage8").show();
+  $(".imageUploadsnipetimage8").show();
+});
+
+$(".savesnipetimage8").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage8").removeClass("editable");
+ 
+  $(".editsnipetimage8").hide();
+  $(".imageUploadsnipetimage8").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage8").change(function() {
+
+    readURLsnipetimg8(this);
+});
+
+});
+function readURLsnipetimg8(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage8').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage8a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage9").hide();
+
+$( ".boxsnipetimage9" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage9").hasClass("editable")) {
+    $(".editsnipetimage9").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage9").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage9").hide();
+
+});
+
+ $(".editsnipetimage9").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage9").addClass("editable");
+   $(".editsnipetimage9").hide();
+  $(".savesnipetimage9").show();
+  $(".imageUploadsnipetimage9").show();
+});
+
+$(".savesnipetimage9").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage9").removeClass("editable");
+ 
+  $(".editsnipetimage9").hide();
+  $(".imageUploadsnipetimage9").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage9").change(function() {
+
+    readURLsnipetimg9(this);
+});
+
+});
+function readURLsnipetimg9(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage9').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage9a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage10").hide();
+
+$( ".boxsnipetimage10" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage10").hasClass("editable")) {
+    $(".editsnipetimage10").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage10").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage10").hide();
+
+});
+
+ $(".editsnipetimage10").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage10").addClass("editable");
+   $(".editsnipetimage10").hide();
+  $(".savesnipetimage10").show();
+  $(".imageUploadsnipetimage10").show();
+});
+
+$(".savesnipetimage10").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage10").removeClass("editable");
+ 
+  $(".editsnipetimage10").hide();
+  $(".imageUploadsnipetimage10").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage10").change(function() {
+
+    readURLsnipetimg10(this);
+});
+
+});
+function readURLsnipetimg10(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage10').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage10a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+
+   $(".imageUploadsnipetimage11").hide();
+
+$( ".boxsnipetimage11" )
+ .on("mouseenter", function() {
+   if ($(".boxsnipetimage11").hasClass("editable")) {
+    $(".editsnipetimage11").hide();
+
+   } 
+   else
+   {
+    $(".editsnipetimage11").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editsnipetimage11").hide();
+
+});
+
+ $(".editsnipetimage11").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage11").addClass("editable");
+   $(".editsnipetimage11").hide();
+  $(".savesnipetimage11").show();
+  $(".imageUploadsnipetimage11").show();
+});
+
+$(".savesnipetimage11").click(function() {
+  $(this).hide();
+  $(".boxsnipetimage11").removeClass("editable");
+ 
+  $(".editsnipetimage11").hide();
+  $(".imageUploadsnipetimage11").hide();
+  
+});
+
+
+
+
+$("#imageUploadsnipetimage11").change(function() {
+
+    readURLsnipetimg11(this);
+});
+
+});
+function readURLsnipetimg11(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewsnipetimage11').attr('src', e.target.result);
+                 $('.imagePreviewsnipetimage11a').attr('data-image', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
--- resources/views/shortcodes/custom/customplain_44.blade.php
+++ resources/views/shortcodes/custom/customplain_44.blade.php
@@ -1,87 +1,30 @@
-<div class="container">
-    <div id="anuCarousel" class="carousel slide" data-ride="carousel">
-    
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active  boxanucarousel1">
-				<span class="editanucarousel1">edit</span>
-		        <span class="saveanucarousel1">save</span>
-                <input type='file' id="imageUploadanucarousel1" accept=".png, .jpg, .jpeg" />
-                <label class="imageUploadanucarousel1" for="imageUploadanucarousel1"></label>
-		        <img id="imagePreviewanucarousel1" src="http://placehold.it/1200x400/cccccc/ffffff">
-         
-
-		 <div class="carousel-caption boxanucarousel5 textanucarousel5">
-		    <span class="editanucarousel5">edit</span>
-		<span class="saveanucarousel5">save</span>
-            <h3>Headline</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <a href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank" class="label label-danger">Bootstrap 3 - Carousel Collection</a></p>
-          </div>
-        </div><!-- End Item -->
- 
- 
- 
-         <div class="item  boxanucarousel2">
-				<span class="editanucarousel2">edit</span>
-		        <span class="saveanucarousel2">save</span>
-                <input type='file' id="imageUploadanucarousel2" accept=".png, .jpg, .jpeg" />
-                <label class="imageUploadanucarousel2" for="imageUploadanucarousel2"></label>
-		        <img id="imagePreviewanucarousel2" src="http://placehold.it/1200x400/999999/cccccc">
-           
-		   
-		   <div class="carousel-caption  boxanucarousel6 textanucarousel6">
-		    <span class="editanucarousel6">edit</span>
-		<span class="saveanucarousel6">save</span>
-            <h3>Headline</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <a href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank" class="label label-danger">Bootstrap 3 - Carousel Collection</a></p>
-          </div>
-        </div><!-- End Item -->
-		
-        
-        <div class="item  boxanucarousel3">
-		<span class="editanucarousel3">edit</span>
-		        <span class="saveanucarousel3">save</span>
-                <input type='file' id="imageUploadanucarousel3" accept=".png, .jpg, .jpeg" />
-                <label class="imageUploadanucarousel3" for="imageUploadanucarousel3"></label>
-          <img id="imagePreviewanucarousel3" src="http://placehold.it/1200x400/dddddd/333333">
-		  
-		  
-           <div class="carousel-caption  boxanucarousel7 textanucarousel7">
-		    <span class="editanucarousel7">edit</span>
-		<span class="saveanucarousel7">save</span>
-            <h3>Headline</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <a href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank" class="label label-danger">Bootstrap 3 - Carousel Collection</a></p>
-          </div>
-        </div><!-- End Item -->
-        
-		
-		
-        <div class="item boxanucarousel4">
-		<span class="editanucarousel4">edit</span>
-		        <span class="saveanucarousel4">save</span>
-                <input type='file' id="imageUploadanucarousel4" accept=".png, .jpg, .jpeg" />
-                <label class="imageUploadanucarousel4" for="imageUploadanucarousel4"></label>
-          <img id="imagePreviewanucarousel4" src="http://placehold.it/1200x400/999999/cccccc">
-		  
-		  
-           <div class="carousel-caption   boxanucarousel8 textanucarousel8">
-		    <span class="editanucarousel8">edit</span>
-		<span class="saveanucarousel8">save</span>
-            <h3>Headline</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-          </div>
-        </div><!-- End Item -->
-                
-      </div><!-- End Carousel Inner -->
-
-
-    	<ul class="nav nav-pills nav-justified">
-          <li data-target="#anuCarousel" data-slide-to="0" class="active"><a href="#">About<small>Lorem ipsum dolor sit</small></a></li>
-          <li data-target="#anuCarousel" data-slide-to="1"><a href="#">Projects<small>Lorem ipsum dolor sit</small></a></li>
-          <li data-target="#anuCarousel" data-slide-to="2"><a href="#">Portfolio<small>Lorem ipsum dolor sit</small></a></li>
-          <li data-target="#anuCarousel" data-slide-to="3"><a href="#">Services<small>Lorem ipsum dolor sit</small></a></li>
-        </ul>
-
-
-    </div><!-- End Carousel -->
-</div>
+<section id="anutabs">
+<div class="tabanu">
+	<div class="container">
+		<h6 class="section-title h1">Tabs</h6>
+		<div class="row">
+			<div class="col-xs-12 ">
+				<nav>
+					<div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+						<a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+						<a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+						<a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="false">About</a>
+					</div>
+				</nav>
+				<div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+					<div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+						Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+					</div>
+					<div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+						Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+					</div>
+					<div class="tab-pane fade" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
+						Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+					</div>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+	</div>
+</section>
