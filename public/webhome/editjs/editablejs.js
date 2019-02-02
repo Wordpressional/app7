12d2b55bac25826b7b7eb433276d61e5229f7984
nagashwini <nagashwini@pyrumas.com>
2019-02-02 13:43:28 +0000

    conflicts edit.js changes

--- public/webhome/editjs/editablejs.js
+++ public/webhome/editjs/editablejs.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let modalId = $('#image-gallery');
 
 $(document)
@@ -764,5 +763,4 @@ if (input.files && input.files[0]) {
         }
   
 }
-=======
->>>>>>> 51d808c38fd569d6a1eefb7e9b8c4c9f3be62950
+
