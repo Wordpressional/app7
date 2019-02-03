75636a13ba7ffb050efb5396553ab6403cfcfbde
Nagashwini <nagashwini@pyrumas.com>
2019-02-03 15:29:20 +0530

    services

--- resources/views/shortcodes/plainhtml/services.blade.php
+++ resources/views/shortcodes/plainhtml/services.blade.php
@@ -4,6 +4,12 @@
 <span class="savemlservicetheme1">save</span>
 <h2>Our Services</h2>
 
+    <span class="editpresentationcolor">Color</span>
+   <span class="contpresentationcolor">
+   
+    <input type="text" name="presentationcolor" value="" class="inputpresentationcolor" placeholder="rgb(91, 22, 22, 1)"><br>
+    <input type="submit" value="Submit" class="submitpresentationcolor">
+    </span>
 </div>
 <section class="presentation">
       
@@ -11,12 +17,18 @@
           <div class="container">
                  
                 <div class="left sidepurple boxmlservicetheme2 textmlservicetheme2">
-                    <span class="service-icon">
-                    <i class="fa fa-desktop" aria-hidden="true">&nbsp;</i>
+                    <span class="service-icon1">
+                    <i class="fa fa-desktop" id="pur" aria-hidden="true">&nbsp;</i>
                     </span>
                     <span class="editmlservicetheme2">edit</span>
   
                       <span class="savemlservicetheme2">save</span>
+                        <span class="editcontpur">fontawesome</span>
+
+                      <span class="contpur">
+                      <input type="text" name="contpur" value="" class="inputcontpur" placeholder="fa fa-desktop"><br>
+                      <input type="submit" value="Submit" class="submitcontpur">
+                      </span>
                     <h4 class="trusted">Trusted worldwide</h4>
                   
                     <p>At lll we build trust among investors and gain the support your ICO needs. Using Blockchain concept we are building trust and transparency into your project. Also make your blockchain project catch the eye and build the trust and transparency of your participants, supporters, and clients. Payments are made using cryptocurrency which is Trustless, Decentralized & Immutable.</p>
@@ -25,17 +37,25 @@
 
                 
          <div class="right sidepurple boxmlservicetheme3 textmlservicetheme3">
-            <span class="service-icon">
-                    <i class="fa fa-desktop" aria-hidden="true">&nbsp;</i>
+            <span class="service-icon2">
+                    <i class="fa fa-desktop" id="pur2" aria-hidden="true">&nbsp;</i>
                     </span>
                     <span class="editmlservicetheme3">edit</span>
   
                       <span class="savemlservicetheme3">save</span>
+                        <span class="editcontpur2">fontawesome</span>
+                        
+                      <span class="contpur2">
+                      <input type="text" name="contpur2" value="" class="inputcontpur2" placeholder="fa fa-desktop"><br>
+                      <input type="submit" value="Submit" class="submitcontpur2">
+                      </span>
                     <h4 class="dev">Secure and Decentralized</h4>
                     
                     <p>Decentralized digital currency systems known as cryptocurrencies are a breakthrough in electronic payments: the absence of a central authority can avoid the risk that a not fully reliable, very small transactions can be made without incurring high costs and transactions can be traced. Furthermore, decentralization provides robustness against many attacks.</p>
                    
                 </div>
+
+
             </div>
 </section>
 
@@ -46,25 +66,53 @@
                   
                     <ul>
                       <li class="feature currencies boxmlservicetheme4 textmlservicetheme4">
-                        <span class="service-icon">
-                    <i class="fa fa-desktop" aria-hidden="true">&nbsp;</i>
+                        <span class="service-icon3">
+                    <i class="fa fa-desktop" id="cur" aria-hidden="true">&nbsp;</i>
                     </span>
                         <span class="editmlservicetheme4">edit</span>
   
                       <span class="savemlservicetheme4">save</span>
+                       <span class="editcontpur3">fontawesome</span>
+
+                      <span class="contpur3">
+                      <input type="text" name="contpur3" value="" class="inputcontpur3" placeholder="fa fa-desktop"><br>
+                      <input type="submit" value="Submit" class="submitcontpur3">
+                      </span>
                           <div class="heading">ICO MARKETING</div>
                             <p>Social media management and cryptocurrency community management are very important to the success of an ICO. The crypto community is knowledgeable, vast, and expanding, and must be tapped into in order to achieve the results you want for your ICO. Getting the trust of the community must be treated as a priority. How the ICO is presented will determine the levels of trust within the community, and ultimately can be the deciding factor between success and failure for your ICO project.</p>
                         </li>
                       <li class="feature sources boxmlservicetheme5 textmlservicetheme5">
-                        <span class="service-icon">
-                    <i class="fa fa-desktop" aria-hidden="true">&nbsp;</i>
+                        <span class="service-icon4">
+                    <i class="fa fa-desktop" id="src" aria-hidden="true">&nbsp;</i>
                     </span>
                          <span class="editmlservicetheme5">edit</span>
   
                       <span class="savemlservicetheme5">save</span>
+                       <span class="editcontpur4">fontawesome</span>
+
+                      <span class="contpur4">
+                      <input type="text" name="contpur4" value="" class="inputcontpur4" placeholder="fa fa-desktop"><br>
+                      <input type="submit" value="Submit" class="submitcontpur4">
+                      </span>
                           <div class="heading">ICO LAUNCH</div>
                             <p>All-inclusive Development support for your ICO Launch. Get Free Quote Now! ICO Consulting. Fast Delivery. Pre ICO Services. Competitive Prices. Whitepaper Drafting. 24 *7 Help & Support.</p>
-                        </li>                     
+                        </li>    
+                        <li class="feature security boxmlservicetheme5a textmlservicetheme5a">
+                        <span class="service-icon5">
+                    <i class="fa fa-desktop" id="sec" aria-hidden="true">&nbsp;</i>
+                    </span>
+                         <span class="editmlservicetheme5a">edit</span>
+  
+                      <span class="savemlservicetheme5a">save</span>
+                       <span class="editcontpur5">fontawesome</span>
+
+                      <span class="contpur5">
+                      <input type="text" name="contpur5" value="" class="inputcontpur5" placeholder="fa fa-desktop"><br>
+                      <input type="submit" value="Submit" class="submitcontpur5">
+                      </span>
+                          <div class="heading">ICO LAUNCH</div>
+                            <p>All-inclusive Development support for your ICO Launch. Get Free Quote Now! ICO Consulting. Fast Delivery. Pre ICO Services. Competitive Prices. Whitepaper Drafting. 24 *7 Help & Support.All-inclusive Development support for your ICO Launch. Get Free Quote Now! ICO Consulting. Fast Delivery. Pre ICO Services. Competitive Prices. Whitepaper Drafting. 24 *7 Help & Support.</p>
+                        </li>                      
                     </ul>
                 
                 </div>
@@ -73,22 +121,51 @@
                    
                     <ul>                      
                       <li class="feature docs boxmlservicetheme6 textmlservicetheme6">
-                        <span class="service-icon">
-                    <i class="fa fa-desktop" aria-hidden="true">&nbsp;</i>
+                        <span class="service-icon6">
+                    <i class="fa fa-desktop" id="doc" aria-hidden="true">&nbsp;</i>
                     </span>
                     <span class="editmlservicetheme6">edit</span>
   
                       <span class="savemlservicetheme6">save</span>
+                       <span class="editcontpur6">fontawesome</span>
+
+                      <span class="contpur6">
+                      <input type="text" name="contpur6" value="" class="inputcontpur6" placeholder="fa fa-desktop"><br>
+                      <input type="submit" value="Submit" class="submitcontpur6">
+                      </span>
                           <div class="heading">SMART CONTRACT </div>
                             <p>Smart contracts go beyond the vending machine in proposing to embed contracts in all sorts of property that is valuable and controlled by digital means. Smart contracts reference that property in a dynamic, often proactively enforced form, and provide much better observation and verification where proactive measures must fall short.proactively enforced form, and provide much better observation and verification where proactive measures must fall short.</p>
                         </li>
                       <li class="feature historical boxmlservicetheme7 textmlservicetheme7">
-                        <span class="service-icon">
-                    <i class="fa fa-desktop" aria-hidden="true">&nbsp;</i>
+                        <span class="service-icon7">
+                    <i class="fa fa-desktop" id="hist" aria-hidden="true">&nbsp;</i>
                     </span>
                         <span class="editmlservicetheme7">edit</span>
   
                       <span class="savemlservicetheme7">save</span>
+                       <span class="editcontpur7">fontawesome</span>
+
+                      <span class="contpur7">
+                      <input type="text" name="contpur7" value="" class="inputcontpur7" placeholder="fa fa-desktop"><br>
+                      <input type="submit" value="Submit" class="submitcontpur7">
+                      </span>
+                          <div class="heading">DAPP DEVELOPMENT</div>
+                            <p>Dapp abbreviated as Decentralized Application used to Develope Applications using Front-end(HTML+CSS+JS) Web page + Back-end(Solidity Smart contract) Programming code in Ethereum platform. Decentralization allows for independent verification and makes it harder to commit fraud.</p>
+                        </li>
+
+                        <li class="feature powerful boxmlservicetheme7a textmlservicetheme7a">
+                        <span class="service-icon8">
+                    <i class="fa fa-desktop" id="pow" aria-hidden="true">&nbsp;</i>
+                    </span>
+                        <span class="editmlservicetheme7a">edit</span>
+  
+                      <span class="savemlservicetheme7a">save</span>
+                       <span class="editcontpur8">fontawesome</span>
+
+                      <span class="contpur8">
+                      <input type="text" name="contpur8" value="" class="inputcontpur8" placeholder="fa fa-desktop"><br>
+                      <input type="submit" value="Submit" class="submitcontpur8">
+                      </span>
                           <div class="heading">DAPP DEVELOPMENT</div>
                             <p>Dapp abbreviated as Decentralized Application used to Develope Applications using Front-end(HTML+CSS+JS) Web page + Back-end(Solidity Smart contract) Programming code in Ethereum platform. Decentralization allows for independent verification and makes it harder to commit fraud.</p>
                         </li>
@@ -99,4 +176,3 @@
          
           </div>
          
-         </section>
