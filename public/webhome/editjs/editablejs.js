b148b4ac66367b08c589b50a60a3807c5cf07e74
nagashwini <nagashwini@pyrumas.com>
2019-02-07 10:01:51 +0000

    edit js and css conflict

--- public/webhome/editjs/editablejs.js
+++ public/webhome/editjs/editablejs.js
@@ -764,3 +764,784 @@ if (input.files && input.files[0]) {
   
 }
 
+
+"use strict";
+var center = { x: 325, y: 175 };
+var serv_dist = 250;
+var pointer_dist = 172;
+var pointer_time = 2;
+var icon_size = 42;
+var circle_radius = 38;
+var text_top_margin = 18;
+var tspan_delta = 16;
+
+//name is used as the title for the bubble
+//icon is the id of the corresponding svg symbol
+var services_data = [{ name: "Menu 1", icon: "industries" }, { name: "Menu 2", icon: "validation" }, { name: "Menu 3", icon: "engineering" }, { name: "Menu 4", icon: "management" }, { name: "Menu 5", icon: "manufacturing" }, { name: "Menu 6", icon: "technical" }, { name: "Menu 7", icon: "process" }];
+
+var services = document.getElementById("service-collection");
+var nav_container = document.getElementById("circle-nav-services");
+var symbol_copy = document.getElementById("circle-nav-copy");
+var use_copy = document.querySelector("use.nav-copy");
+
+//Keeps code DRY avoiding namespace for element creation
+function createSVGElement(el) {
+  return document.createElementNS("http://www.w3.org/2000/svg", el);
+}
+
+//Quick setup for multiple attributes
+function setAttributes(el, options) {
+  Object.keys(options).forEach(function (attr) {
+    el.setAttribute(attr, options[attr]);
+  });
+}
+
+//Service bubbles are created dynamically
+function addService(serv, index) {
+  var group = createSVGElement("g");
+  group.setAttribute("class", "service serv-" + index);
+
+  /* This group is needed to apply animations in
+    the icon and its background at the same time */
+  var icon_group = createSVGElement("g");
+  icon_group.setAttribute("class", "icon-wrapper");
+
+  var circle = createSVGElement("circle");
+  setAttributes(circle, {
+    r: circle_radius,
+    cx: center.x,
+    cy: center.y
+  });
+  var circle_shadow = circle.cloneNode();
+  setAttributes(circle, {
+    class: 'shadow'
+  });
+  icon_group.appendChild(circle_shadow);
+  icon_group.appendChild(circle);
+
+  var symbol = createSVGElement("use");
+  setAttributes(symbol, {
+    'x': center.x - icon_size / 2,
+    'y': center.y - icon_size / 2,
+    'width': icon_size,
+    'height': icon_size
+  });
+  symbol.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#" + serv.icon);
+  icon_group.appendChild(symbol);
+
+  group.appendChild(icon_group);
+
+  var text = createSVGElement("text");
+  setAttributes(text, {
+    x: center.x,
+    y: center.y + circle_radius + text_top_margin
+  });
+
+  var tspan = createSVGElement("tspan");
+  if (serv.name.indexOf('\n') >= 0) {
+
+    var tspan2 = tspan.cloneNode();
+    var name = serv.name.split('\n');
+    jQuery(tspan).text(name[0]);
+    jQuery(tspan2).text(name[1]);
+
+    setAttributes(tspan2, {
+      x: center.x,
+      dy: tspan_delta
+    });
+
+    text.appendChild(tspan);
+    text.appendChild(tspan2);
+  } else {
+    jQuery(tspan).text(serv.name);
+    text.appendChild(tspan);
+  }
+
+  group.appendChild(text);
+  services.appendChild(group);
+
+  var service_bubble = jQuery(".serv-" + index);
+
+  //Uses tween to look for right position
+  twn_pivot_path.seek(index);
+  TweenLite.set(service_bubble, {
+    x: pivot_path.x,
+    y: pivot_path.y,
+    transformOrigin: "center center"
+  });
+
+  service_bubble.click(serviceClick);
+}
+
+//Creates pointer
+function createPointer() {
+  var service_pointer = createSVGElement("circle");
+
+  setAttributes(service_pointer, {
+    cx: center.x - pointer_dist,
+    cy: center.y,
+    r: 12,
+    class: "pointer"
+  });
+
+  nav_container.appendChild(service_pointer);
+
+  service_pointer = document.querySelector("svg .pointer");
+
+  var pointer_circle = [{ x: 0, y: 0 }, { x: pointer_dist, y: pointer_dist }, { x: pointer_dist * 2, y: 0 }, { x: pointer_dist, y: -pointer_dist }, { x: 0, y: 0 }];
+
+  twn_pointer_path.to(service_pointer, pointer_time, {
+    bezier: {
+      values: pointer_circle,
+      curviness: 1.5 },
+    ease: Power0.easeNone,
+    transformOrigin: "center center"
+  });
+}
+
+//Defines behavior for service bubbles
+function serviceClick(ev) {
+
+  //There's always an active service
+  jQuery(".service.active").removeClass("active");
+
+  var current = jQuery(ev.currentTarget);
+  current.addClass("active");
+
+  //There's a "serv-*" class for each bubble
+  var current_class = current.attr("class").split(" ")[1];
+  var class_index = current_class.split('-')[1];
+
+  //Hides current text of the main bubble
+  jQuery(use_copy).addClass("changing");
+
+  //Sets pointer to the right position
+  twn_pointer_path.tweenTo(class_index * (pointer_time / (2 * 6)));
+
+  //After it's completely hidden, the text changes and becomes visible
+  setTimeout(function () {
+    var viewBoxY = 300 * class_index;
+    symbol_copy.setAttribute("viewBox", "0 " + viewBoxY + " 300 300");
+    jQuery(use_copy).removeClass("changing");
+  }, 250);
+}
+
+//Array describes points for a whole circle in order to get
+//the right curve
+var half_circle = [{ x: -serv_dist, y: 0 }, { x: 0, y: serv_dist }, { x: serv_dist, y: 0 }, { x: 0, y: -serv_dist }, { x: -serv_dist, y: 0 }];
+
+//A simple object is used in the tween to retrieve its values
+var pivot_path = { x: half_circle[0].x, y: half_circle[0].y };
+
+//The object is animated with a duration based on how many bubbles
+//should be placed
+var twn_pivot_path = TweenMax.to(pivot_path, 12, {
+  bezier: {
+    values: half_circle,
+    curviness: 1.5
+  },
+  ease: Linear.easeNone
+});
+
+services_data.reduce(function (count, serv) {
+  addService(serv, count);
+  return ++count;
+}, 0);
+
+//The variable is modified inside the function
+//but its also used later to toggle its class
+var twn_pointer_path = new TimelineMax({ paused: true });
+createPointer();
+
+//Adding it immediately triggers a bug for the transform
+setTimeout(function () {
+  return jQuery("#service-collection .serv-0").addClass("active");
+}, 200);
+//# sourceURL=pen.js
+
+
+
+$(document).ready(function(){
+
+   $(".imageUploadteam1").hide();
+
+$( ".boxteam1" )
+ .on("mouseenter", function() {
+   if ($(".boxteam1").hasClass("editable")) {
+    $(".editteam1").hide();
+
+   } 
+   else
+   {
+    $(".editteam1").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam1").hide();
+
+});
+
+ $(".editteam1").click(function() {
+  $(this).hide();
+  $(".boxteam1").addClass("editable");
+   $(".editteam1").hide();
+  $(".saveteam1").show();
+  $(".imageUploadteam1").show();
+});
+
+$(".saveteam1").click(function() {
+  $(this).hide();
+  $(".boxteam1").removeClass("editable");
+ 
+  $(".editteam1").hide();
+  $(".imageUploadteam1").hide();
+  
+});
+
+
+
+
+$("#imageUploadteam1").change(function() {
+
+    readURLteam1(this);
+});
+
+});
+function readURLteam1(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewteam1').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+
+$(document).ready(function(){
+$( ".boxteam2" )
+ .on("mouseenter", function() {
+   if ($(".boxteam2").hasClass("editable")) {
+    $(".editteam2").hide();
+
+   } 
+   else
+   {
+    $(".editteam2").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam2").hide();
+
+});
+
+ $(".editteam2").click(function() {
+  $(this).hide();
+  $(".boxteam2").addClass("editable");
+  $(".textteam2").attr("contenteditable", "true");
+   $(".editteam2").hide();
+  $(".saveteam2").show();
+ 
+});
+
+$(".saveteam2").click(function() {
+  $(this).hide();
+  $(".boxteam2").removeClass("editable");
+ $(".textteam2").removeAttr("contenteditable");
+  $(".editteam2").hide();
+  
+});
+})
+
+$(document).ready(function(){
+
+   $(".imageUploadteam3").hide();
+
+$( ".boxteam3" )
+ .on("mouseenter", function() {
+   if ($(".boxteam3").hasClass("editable")) {
+    $(".editteam3").hide();
+
+   } 
+   else
+   {
+    $(".editteam3").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam3").hide();
+
+});
+
+ $(".editteam3").click(function() {
+  $(this).hide();
+  $(".boxteam3").addClass("editable");
+   $(".editteam3").hide();
+  $(".saveteam3").show();
+  $(".imageUploadteam3").show();
+});
+
+$(".saveteam3").click(function() {
+  $(this).hide();
+  $(".boxteam3").removeClass("editable");
+ 
+  $(".editteam3").hide();
+  $(".imageUploadteam3").hide();
+  
+});
+
+
+
+
+$("#imageUploadteam3").change(function() {
+
+    readURLteam3(this);
+});
+
+});
+function readURLteam3(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewteam3').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+$( ".boxteam4" )
+ .on("mouseenter", function() {
+   if ($(".boxteam4").hasClass("editable")) {
+    $(".editteam4").hide();
+
+   } 
+   else
+   {
+    $(".editteam4").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam4").hide();
+
+});
+
+ $(".editteam4").click(function() {
+  $(this).hide();
+  $(".boxteam4").addClass("editable");
+  $(".textteam4").attr("contenteditable", "true");
+   $(".editteam4").hide();
+  $(".saveteam4").show();
+ 
+});
+
+$(".saveteam4").click(function() {
+  $(this).hide();
+  $(".boxteam4").removeClass("editable");
+ $(".textteam4").removeAttr("contenteditable");
+  $(".editteam4").hide();
+  
+});
+})
+
+$(document).ready(function(){
+
+   $(".imageUploadteam5").hide();
+
+$( ".boxteam5" )
+ .on("mouseenter", function() {
+   if ($(".boxteam5").hasClass("editable")) {
+    $(".editteam5").hide();
+
+   } 
+   else
+   {
+    $(".editteam5").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam5").hide();
+
+});
+
+ $(".editteam5").click(function() {
+  $(this).hide();
+  $(".boxteam5").addClass("editable");
+   $(".editteam5").hide();
+  $(".saveteam5").show();
+  $(".imageUploadteam5").show();
+});
+
+$(".saveteam5").click(function() {
+  $(this).hide();
+  $(".boxteam5").removeClass("editable");
+ 
+  $(".editteam5").hide();
+  $(".imageUploadteam5").hide();
+  
+});
+
+
+
+
+$("#imageUploadteam5").change(function() {
+
+    readURLteam5(this);
+});
+
+});
+function readURLteam5(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewteam5').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+
+$(document).ready(function(){
+$( ".boxteam6" )
+ .on("mouseenter", function() {
+   if ($(".boxteam6").hasClass("editable")) {
+    $(".editteam6").hide();
+
+   } 
+   else
+   {
+    $(".editteam6").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam6").hide();
+
+});
+
+ $(".editteam6").click(function() {
+  $(this).hide();
+  $(".boxteam6").addClass("editable");
+  $(".textteam6").attr("contenteditable", "true");
+   $(".editteam6").hide();
+  $(".saveteam6").show();
+ 
+});
+
+$(".saveteam6").click(function() {
+  $(this).hide();
+  $(".boxteam6").removeClass("editable");
+ $(".textteam6").removeAttr("contenteditable");
+  $(".editteam6").hide();
+  
+});
+})
+
+
+
+$(document).ready(function(){
+
+   $(".imageUploadteam7").hide();
+
+$( ".boxteam7" )
+ .on("mouseenter", function() {
+   if ($(".boxteam7").hasClass("editable")) {
+    $(".editteam7").hide();
+
+   } 
+   else
+   {
+    $(".editteam7").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam7").hide();
+
+});
+
+ $(".editteam7").click(function() {
+  $(this).hide();
+  $(".boxteam7").addClass("editable");
+   $(".editteam7").hide();
+  $(".saveteam7").show();
+  $(".imageUploadteam7").show();
+});
+
+$(".saveteam7").click(function() {
+  $(this).hide();
+  $(".boxteam7").removeClass("editable");
+ 
+  $(".editteam7").hide();
+  $(".imageUploadteam7").hide();
+  
+});
+
+
+
+
+$("#imageUploadteam7").change(function() {
+
+    readURLteam7(this);
+});
+
+});
+function readURLteam7(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewteam7').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+$( ".boxteam8" )
+ .on("mouseenter", function() {
+   if ($(".boxteam8").hasClass("editable")) {
+    $(".editteam8").hide();
+
+   } 
+   else
+   {
+    $(".editteam8").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam8").hide();
+
+});
+
+ $(".editteam8").click(function() {
+  $(this).hide();
+  $(".boxteam8").addClass("editable");
+  $(".textteam8").attr("contenteditable", "true");
+   $(".editteam8").hide();
+  $(".saveteam8").show();
+ 
+});
+
+$(".saveteam8").click(function() {
+  $(this).hide();
+  $(".boxteam8").removeClass("editable");
+ $(".textteam8").removeAttr("contenteditable");
+  $(".editteam8").hide();
+  
+});
+})
+
+$(document).ready(function(){
+
+   $(".imageUploadteam9").hide();
+
+$( ".boxteam9" )
+ .on("mouseenter", function() {
+   if ($(".boxteam9").hasClass("editable")) {
+    $(".editteam9").hide();
+
+   } 
+   else
+   {
+    $(".editteam9").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam9").hide();
+
+});
+
+ $(".editteam9").click(function() {
+  $(this).hide();
+  $(".boxteam9").addClass("editable");
+   $(".editteam9").hide();
+  $(".saveteam9").show();
+  $(".imageUploadteam9").show();
+});
+
+$(".saveteam9").click(function() {
+  $(this).hide();
+  $(".boxteam9").removeClass("editable");
+ 
+  $(".editteam9").hide();
+  $(".imageUploadteam9").hide();
+  
+});
+
+
+
+
+$("#imageUploadteam9").change(function() {
+
+    readURLteam9(this);
+});
+
+});
+function readURLteam9(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewteam9').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+
+$(document).ready(function(){
+$( ".boxteam10" )
+ .on("mouseenter", function() {
+   if ($(".boxteam10").hasClass("editable")) {
+    $(".editteam8").hide();
+
+   } 
+   else
+   {
+    $(".editteam10").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam10").hide();
+
+});
+
+ $(".editteam10").click(function() {
+  $(this).hide();
+  $(".boxteam10").addClass("editable");
+  $(".textteam10").attr("contenteditable", "true");
+   $(".editteam10").hide();
+  $(".saveteam10").show();
+ 
+});
+
+$(".saveteam10").click(function() {
+  $(this).hide();
+  $(".boxteam10").removeClass("editable");
+ $(".textteam10").removeAttr("contenteditable");
+  $(".editteam10").hide();
+  
+});
+})
+
+$(document).ready(function(){
+
+   $(".imageUploadteam11").hide();
+
+$( ".boxteam11" )
+ .on("mouseenter", function() {
+   if ($(".boxteam11").hasClass("editable")) {
+    $(".editteam11").hide();
+
+   } 
+   else
+   {
+    $(".editteam11").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam11").hide();
+
+});
+
+ $(".editteam11").click(function() {
+  $(this).hide();
+  $(".boxteam11").addClass("editable");
+   $(".editteam11").hide();
+  $(".saveteam11").show();
+  $(".imageUploadteam11").show();
+});
+
+$(".saveteam11").click(function() {
+  $(this).hide();
+  $(".boxteam11").removeClass("editable");
+ 
+  $(".editteam11").hide();
+  $(".imageUploadteam11").hide();
+  
+});
+
+
+
+
+$("#imageUploadteam11").change(function() {
+
+    readURLteam11(this);
+});
+
+});
+function readURLteam11(input) {
+if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            //console.log(input.files[0]);
+            reader.onload = function (e) {
+                $('#imagePreviewteam11').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+  
+}
+$(document).ready(function(){
+$( ".boxteam12" )
+ .on("mouseenter", function() {
+   if ($(".boxteam12").hasClass("editable")) {
+    $(".editteam12").hide();
+
+   } 
+   else
+   {
+    $(".editteam12").show();
+   }
+  
+})
+.on("mouseleave", function() {
+  
+  $(".editteam12").hide();
+
+});
+
+ $(".editteam12").click(function() {
+  $(this).hide();
+  $(".boxteam12").addClass("editable");
+  $(".textteam12").attr("contenteditable", "true");
+   $(".editteam12").hide();
+  $(".saveteam12").show();
+ 
+});
+
+$(".saveteam12").click(function() {
+  $(this).hide();
+  $(".boxteam12").removeClass("editable");
+ $(".textteam12").removeAttr("contenteditable");
+  $(".editteam12").hide();
+  
+});
+})
--- resources/views/shortcodes/custom/customplain_96.blade.php
+++ resources/views/shortcodes/custom/customplain_96.blade.php
@@ -1,114 +1,72 @@
-<body>
-	<div class="pradeepmainsn3" >
-<div class="container">
+<section class="login-block">
+  <div class=pradeepsubscribe>
+    <div class="container">
 	<div class="row">
-	<ul>
-		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab  item">
-			<div class="folded-corner service_tab_1">
-				<div class="text">
-					<i class="fa fa-image fa-5x fa-icon-image">&nbsp</i>
-						<p class="item-title">
-								<h3 style="font-size: 24px;"> Designing</h3>
-							</p><!-- /.item-title -->
-					<p>
-						This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, 
-					</p>
-				</div>
-			</div>
-	    </div>
-		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
-			<div class="folded-corner service_tab_1">
-				<div class="text">
-					<i class="fa fa-lightbulb-o fa-5x fa-icon-image" >nbsp</i>
-						<p class="item-title">
-							<h3 style="font-size: 24px;"> Developing</h3>
-						</p><!-- /.item-title -->
-						<p>
-							This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, 
-					</p>
-				</div>
-			</div>
+		<div class="col-md-4 login-sec">
+		    <h2 class="text-center">Subscribe Now</h2>
+		    <form class="login-form">
+  <div class="form-group">
+    <label for="exampleInputEmail1" class="text-uppercase">Name</label>
+    <input type="text" class="form-control" placeholder="">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1" class="text-uppercase">Email</label>
+    <input type="Email" class="form-control" placeholder="">
+  </div>
+  
+  
+    <div class="form-check">
+   <!--  <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      <small>Remember Me</small>
+    </label> -->
+    <button type="submit" class="btn btn-login float-right">Subscribe me</button>
+  </div>
+  
+</form>
+<div class="copy-text">Created with <i class="fa fa-heart">&nbsp</i> by <a href="http://pyrumas.com">pyrumas software pvt ltd</a></div>
 		</div>
-		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
-			<div class="folded-corner service_tab_1">
-				<div class="text">
-					<i class="fa fa-truck fa-5x fa-icon-image">&nbsp</i>
-						<p class="item-title">
-							<h3 style="font-size: 24px;"> Marketing</h3>
-						</p><!-- /.item-title -->
-					<p>
-						This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, 
-					</p>
-				</div>
-			</div>
+		<div class="col-md-8 banner-sec">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                 <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+            <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>	
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>	
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images.pexels.com/photos/872957/pexels-photo-872957.jpeg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>	
+    </div>
+  </div>
+            </div>	   
+		    
 		</div>
-		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
-			<div class="folded-corner service_tab_1">
-				<div class="text">
-					<i class="fa fa-diamond fa-5x fa-icon-image">&nbsp</i>
-						<p class="item-title">
-							<h3 style="font-size: 24px;"> Branding</h3>
-						</p><!-- /.item-title -->
-					<p>
-						This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, 
-					</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
-			<div class="folded-corner service_tab_1">
-			<div class="text">
-				<i class="fa fa-line-chart fa-5x fa-icon-image">&nbsp</i>
-					<p class="item-title">
-						<h3 style="font-size: 24px;">Analytics</h3>
-					</p><!-- /.item-title -->
-					<p>
-						This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, 
-					</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
-			<div class="folded-corner service_tab_1">
-				<div class="text">
-					<i class="fa fa-mobile fa-5x fa-icon-image">&nbsp</i>
-						<p class="item-title">
-							<h3 style="font-size: 24px;">Mobil Apps</h3>
-						</p><!-- /.item-title -->
-					<p>
-						This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, 
-					</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
-				<div class="folded-corner service_tab_1">
-					<div class="text">
-						<i class="fa fa-money fa-5x fa-icon-image">&nbsp</i>
-							<p class="item-title">
-								<h3 style="font-size: 24px;"> e-commerce</h3>
-							</p><!-- /.item-title -->
-						<p>
-							This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, 
-						</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 Services-tab item">
-			<div class="folded-corner service_tab_1">
-				<div class="text">
-					<i class="fa fa-bullhorn fa-5x fa-icon-image">&nbsp</i>
-						<p class="item-title">
-							<h3 style="font-size: 24px;"> Support</h3>
-						</p><!-- /.item-title -->
-					<p>
-						This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, 
-					</p>
-				</div>
-			</div>
-		</div>
-	   </ul>
 	</div>
 </div>
-
-  </div>
+</div>
+</div>
+</section>
