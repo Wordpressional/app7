0673cdbca0f573d4ac02cda7228682d12e13ef21
Nagashwini <nagashwini@pyrumas.com>
2019-02-07 15:54:08 +0530

    testing mail using nodemailer

--- public/js/common.js
+++ public/js/common.js
@@ -525,7 +525,7 @@ $('#testmail').click(function(){
   
 alert("clicked");
     $.ajax({
-        url: "{{url('/')}}:8123/",
+        url: "http://139.59.47.15:8123/",
         dataType: "text",
          success: function(data) {
           alert("Sent email successfully");
