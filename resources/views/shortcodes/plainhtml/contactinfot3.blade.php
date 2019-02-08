f6e1a4e364f9e32a45327d036ff1063513830a27
Nagashwini <nagashwini@pyrumas.com>
2019-02-08 18:34:50 +0530

    widgets code changes

--- resources/views/shortcodes/plainhtml/contactinfot3.blade.php
+++ resources/views/shortcodes/plainhtml/contactinfot3.blade.php
@@ -22,11 +22,11 @@
   
                
                 <span class="contmllinkedin27">
-                <input type="text" name="inputmlthemeone27" class="inputmlthemeone27" placeholder="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100939.98555098464!2d-122.507640204439!3d37.757814996609724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sro!4v1498231462606">
+                <input type="text" name="inputmlthemeone27" class="inputmlthemeone27" placeholder="//www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100939.98555098464!2d-122.507640204439!3d37.757814996609724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sro!4v1498231462606">
                 <input type="submit" value="Submit" class="submitmlthemeone27">
                 </span>
                     <div class="map-responsive">
-                        <iframe class="hrefchangemllinkedin27" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100939.98555098464!2d-122.507640204439!3d37.757814996609724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sro!4v1498231462606" allowfullscreen></iframe>
+                        <iframe class="hrefchangemllinkedin27" src="//www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100939.98555098464!2d-122.507640204439!3d37.757814996609724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sro!4v1498231462606" allowfullscreen></iframe>
                     </div>
                 </div>
                 </div> <!-- end of col -->
