bec850680fb3605bffca10c8fb940142476ebabc
Nagashwini <nagashwini@pyrumas.com>
2019-02-19 08:46:02 +0530

    local mail working as per requirement partially

---
+++ resources/views/webhome/mailfunc.blade.php
@@ -0,0 +1,33 @@
+<script>
+$( document ).ready(function() {
+// $('#testmail').click(function(){
+// var mfileconfname = document.getElementsByClassName('mfileconfname').val();  
+//   var data = JSON.stringify({
+//       _token:String($('meta[name="csrf-token"]').attr('content')),
+//       mailconf1:String("hi"),
+//       mailconf2:String("bye")
+      
+//   });
+
+// //alert("clicked");
+// if(mfileconfname == "mconfig1.txt"){
+//     $.ajax({
+//         //url: "https://pyrupay.com/mailapp3/mymail",
+//         url: "http://localhost:8123/mymail1"
+//         dataType: "text",
+//         type: 'post',
+//         data:  data,
+//          success: function(data) {
+//           //alert("Sent email successfully using pyrupay");
+           
+//         },
+       
+//     });
+    
+//  }
+//  });
+
+console.log("hiiiiiiiii");
+
+});
+</script>
