6a60850ad954b8cd6854ca8979e34612a515df06
Nagashwini <nagashwini@pyrumas.com>
2019-02-25 13:17:05 +0530

    contact testmail errors

---
+++ resources/views/shortcodes/plainhtml/aboutt5.blade.php
@@ -0,0 +1,57 @@
+
+ <!-- Start home-about Area -->
+<section class="mpoltheme home-about-area section-gap relative boxmpoltheme9" id="mpolabout">	
+
+<span class="editmpoltheme9">edit</span>
+              
+               <span class="savempoltheme9">save</span>
+           <span class="labelmpoltheme9"><label class="imageUploadmpoltheme9" for="imageUploadmpoltheme9"></label>
+
+            
+           </span>
+
+            <input type='file' id="imageUploadmpoltheme9" accept=".png, .jpg, .jpeg" />		
+	<div class="container mycont boxmpoltheme8 textmpoltheme8">
+		  <span class="editmpoltheme8">edit</span>
+
+               <span class="savempoltheme8">save</span>
+
+		<div class="row align-items-center justify-content-end">
+			<div class="col-lg-6 no-padding home-about-right">
+				<h1>
+					Who we are <br>
+					to Serve the nation
+				</h1>
+				<p>
+					inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach.
+				</p>
+				<div class="row no-gutters	">
+					<div class="single-services col">
+						
+						<a href="#">
+							<h4>Expert Services</h4>
+						</a>
+						<p>
+							Usage of the Internet is becoming more common due to rapid advancement of technology.
+						</p>
+					</div>
+					<div class="single-services col">
+						
+						<a href="#">
+							<h4>Great Support</h4>
+						</a>
+						<p>
+							Usage of the Internet is becoming more common due to rapid advancement of technology.
+						</p>
+					</div>								
+				</div>
+			</div>
+
+		</div>
+
+	</div>	
+	<div class="myimg"></div>
+
+</section>
+<!-- End home-about Area -->
+
---
+++ resources/views/shortcodes/plainhtml/bannert5.blade.php
@@ -0,0 +1,39 @@
+
+ <!-- start banner Area -->
+<section class="mpoltheme banner-area relative boxmpoltheme2 boxmpoltheme222" id="home">
+	   <span class="contmpolbg2">
+            <input type="text" name="mpolbg2"  class="inputmpolbg2" placeholder="//images/slider/1.jpg"><br>
+
+         <input type="submit" value="Submit" class="submitmpolbg2">
+            </span>
+                 <span class="bgmpoltheme2">image
+                 </span>
+<span class="editmpoltheme2">edit</span>
+              
+               <span class="savempoltheme2">save</span>
+           <span class="labelmpoltheme2"><label class="imageUploadmpoltheme2" for="imageUploadmpoltheme2"></label>
+
+            
+           </span>
+
+            <input type='file' id="imageUploadmpoltheme2" accept=".png, .jpg, .jpeg" />
+	<div class="overlay overlay-bg"></div>
+	<div class="container">
+		<div class="row fullscreen d-flex align-items-center justify-content-center">
+			<div class="banner-content col-lg-12 col-md-12 boxmpoltheme3 textmpoltheme3">
+				  <span class="editmpoltheme3">edit</span>
+              
+               <span class="savempoltheme3">save</span>
+				<h6 class="text-uppercase">Don’t look further, This is our Leader</h6>
+				<h1>
+					Support Our Party			
+				</h1>
+				<p class="text-white">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et <br> dolore magna aliqua. Ut enim ad minim.  sed do eiusmod tempor incididunt.
+				</p>
+				<a href="#" class="primary-btn header-btn text-uppercase">Get Started</a>
+			</div>												
+		</div>
+	</div>
+</section>
+<!-- End banner Area -->
