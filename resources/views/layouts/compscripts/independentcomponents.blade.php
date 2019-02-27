c241206e16c7bcb53971cd43bbcbc9abea3f5431
Nagashwini <nagashwini@pyrumas.com>
2019-02-27 13:23:46 +0530

    contactus img

--- resources/views/layouts/compscripts/independentcomponents.blade.php
+++ resources/views/layouts/compscripts/independentcomponents.blade.php
@@ -18472,7 +18472,7 @@ if (input.files && input.files[0]) {
             var reader = new FileReader();
             console.log(input.files[0]);
             reader.onload = function (e) {
-               $('#contact-us').css('background-image', 'url('+e.target.result +')');
+               $('#moxycontact-us').css('background-image', 'url('+e.target.result +')');
                
             }
             reader.readAsDataURL(input.files[0]);
