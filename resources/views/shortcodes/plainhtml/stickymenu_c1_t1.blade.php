97082bac8aea8cc7a962e4f5ee8de75d6e476336
Nagashwini <nagashwini@pyrumas.com>
2019-03-09 07:22:55 +0530

    style changes

--- resources/views/shortcodes/plainhtml/stickymenu_c1_t1.blade.php
+++ resources/views/shortcodes/plainhtml/stickymenu_c1_t1.blade.php
@@ -1,24 +1,17 @@
-<!-- HEADER -->
 <header class="main_h sticky">
 	<div class="themeone">
-
-    <div class="row">
- 	
+    <div class="row"> 	
  <nav class="navbar navbar-expand-sm">
 		<div  class="boxlogolnndynamic">
         <span class="editlogolnndynamic">edit</span>
         <span class="savelogolnndynamic">save</span>
         <input type='file' id="imageUploadlogonlnndynamic" accept=".png, .jpg, .jpeg" />
-        <label class="imageUploadlogonlnndynamic" for="imageUploadlogonlnndynamic">&nbsp;</label>
-        
+        <label class="imageUploadlogonlnndynamic" for="imageUploadlogonlnndynamic">&nbsp;</label>        
         <span ><a href="{{url('/')}}"><img id="imagePreviewlogolnndynamic" src="{{asset('themes/t1/logo2.png')}}" alt="Logo"></a></span>
-        </div>
-
-		     
+        </div>		     
 		 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="lnr-menu"></span>
         </button>
-
 		  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     		<ul class="navbar-nav mr-auto">
 		                <li><a href="#home">Home</a></li>
@@ -31,8 +24,5 @@
 		        </nav>
 		        <!-- END NAVBAR -->
 		    </div> <!-- / row -->
-		</div>
-	
+		</div>	
 		</header>
-	<!-- END HEADER -->
-
