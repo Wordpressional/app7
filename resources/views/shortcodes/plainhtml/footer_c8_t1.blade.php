aea37c1ce184fbe18835748b30d5d31ac526cf62
Nagashwini <nagashwini@pyrumas.com>
2019-03-10 02:16:36 +0530

    style btn type is button

--- resources/views/shortcodes/plainhtml/footer_c8_t1.blade.php
+++ resources/views/shortcodes/plainhtml/footer_c8_t1.blade.php
@@ -30,7 +30,7 @@
                   <li>
                   	<span class="contmfooterfacebook2">
                     <input type="text" name="mfooterfacebook" value="" class="inputmfooterfacebook2" placeholder="http://facebook.com"><br>
-                    <input type="submit" value="Submit" class="submitmfooterfacebook2">
+                    <input type="button" value="Submit" class="submitmfooterfacebook2">
                     </span>
                 <span class="boxmlfooterbladethemeone2">
                   <span class="editmlfooterbladethemeone2">edit</span>
@@ -45,7 +45,7 @@
                   <li>
                   	<span class="contmfootertwitter3">
                   <input type="text" name="mfootertwitter" value="" class="inputmfootertwitter3" placeholder="http://twitter.com"><br>
-                  <input type="submit" value="Submit" class="submitmfootertwitter3">
+                  <input type="button" value="Submit" class="submitmfootertwitter3">
                     </span>
                   <span class="boxmlfooterbladethemeone3">
                   <span class="editmlfooterbladethemeone3">edit</span>
@@ -60,7 +60,7 @@
                 <li>
                 	 <span class="contmfooterrss4">
                   <input type="text" name="mfooterrss" value="" class="inputmfooterrss4" placeholder="http://rss.com"><br>
-                  <input type="submit" value="Submit" class="submitmfooterrss4"">
+                  <input type="button" value="Submit" class="submitmfooterrss4"">
                     </span>
                   <span class="boxmlfooterbladethemeone4">
                   <span class="editmlfooterbladethemeone4">edit</span>
@@ -76,7 +76,7 @@
                 <li>
                 	 <span class="contmfooteryoutube5">
                   <input type="text" name="mfooteryoutube" value="" class="inputmfooteryoutube5" placeholder="http://youtube.com"><br>
-                  <input type="submit" value="Submit" class="submitmfooteryoutube5"">
+                  <input type="button" value="Submit" class="submitmfooteryoutube5"">
                   </span>
 
                    <span class="boxmlfooterbladethemeone5">
@@ -94,7 +94,7 @@
                 <li>
                 	 <span class="contmfooterlinkedin6">
                   <input type="text" name="mfooterlinkedin" value="" class="inputmfooterlinkedin6" placeholder="http://linkedin.com"><br>
-                  <input type="submit" value="Submit" class="submitmfooterlinkedin6">
+                  <input type="button" value="Submit" class="submitmfooterlinkedin6">
                   </span>
                   <span class="boxmlfooterbladethemeone6">
                    <span class="editmlfooterbladethemeone6">edit</span> 
@@ -110,7 +110,7 @@
                 <li>
                 	 <span class="contmfootergoogleplus7">
                   <input type="text" name="mfootergoogleplus" value="" class="inputmfootergoogleplus7" placeholder="http://googleplus.com"><br>
-                  <input type="submit" value="Submit" class="submitmfootergoogleplus7">
+                  <input type="button" value="Submit" class="submitmfootergoogleplus7">
                   </span>
                    <span class="boxmlfooterbladethemeone7">
                    <span class="editmlfooterbladethemeone7">edit</span>
@@ -135,7 +135,7 @@
   <span class="contpresentationcolorcnfp1">
   <input type="text" data-cke-saved-name="c2presentationcolor" name="c2presentationcolor" value="" placeholder="rgb(91, 22, 22, 1)" data-cke-editable="1" contenteditable="false" class="inputpresentationcolorcnfp1">
   <br>
-  <input type="submit" value="Submit" data-cke-editable="1" contenteditable="false" class="submitpresentationcolorcnfp1">
+  <input type="button" value="Submit" data-cke-editable="1" contenteditable="false" class="submitpresentationcolorcnfp1">
   </span>
 			<div class="row">
 				<div class="col-lg-12">
