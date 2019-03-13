69bb8c9d923e018d7ab5ba2a1c989fc307891167
Nagashwini <nagashwini@pyrumas.com>
2019-03-13 21:27:39 +0530

    load theme

--- resources/views/admin/themes/loadtheme.blade.php
+++ resources/views/admin/themes/loadtheme.blade.php
@@ -531,7 +531,7 @@ function random_color() {
         }
         if(defCalls1)
         {
-          alert(defCalls1);
+          alert(http_status(defCalls1));
           //alert(xyz);
           if(xyz == 2){ 
           var defCalls2 = setajax(arrasso2);
@@ -542,7 +542,7 @@ function random_color() {
         
         if(defCalls2)
         {
-          alert(defCalls2);
+          alert(http_status(defCalls2));
           //alert(xyz);
           if(xyz == 3){ 
           var defCalls3 = setajax(arrasso3);
