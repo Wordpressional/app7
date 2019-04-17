b290c517e57e1784d4b14dc839519d5ea8a4d98e
Nagashwini <nagashwini@pyrumas.com>
2019-04-17 10:33:47 +0530

    viewport added

--- resources/views/layoutsecom/front/mythemeapp.blade.php
+++ resources/views/layoutsecom/front/mythemeapp.blade.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  
+  <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link href="{{ asset('webhome/css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/ecomm/css/style.min.css') }}" rel="stylesheet">
 
